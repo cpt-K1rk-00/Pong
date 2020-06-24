8436e44ac5029f8ddf448ab18928eab1451282cc
Jan Pandikow <jpandikow2002@gmail.com>
2020-06-24 18:44:14 +0200

    Player Movement

--- game.py
+++ game.py
@@ -11,31 +11,36 @@ class Player(pygame.sprite.Sprite):
 		self.image.fill(color)
 		self.rect = self.image.get_rect()
 		self.rect.topleft = initial_position
+		self.speed = 1
 
 	# Move function to move the player down to the edge
-	def move_down():
-		pass
+	def move_down(self):
+		if self.rect.topleft[1] < 250:
+			self.rect.topleft = (self.rect.topleft[0], self.rect.topleft[1] + self.speed)
 
 	# Move function to move the player up to the edge
-	def move_up():
-		pass
+	def move_up(self):
+		if self.rect.topleft[1] > 0:
+			self.rect.topleft = (self.rect.topleft[0], self.rect.topleft[1] - self.speed)
 
 
 def main():
+	# zähler - bei jedem fünften Frame wird der User-Input verarbeitet
+	cnt = 0
+
 	# Die Spieler erstellen
 	player_1 = Player((255, 255, 255), 10, 100, (2, 0))
 	player_2 = Player((255, 255, 255), 10, 100, (488,0))
 
 	# Pygame-Mododule initialisieren
-	# Fenster erstellend
+	# Fenster erstellen
 	pygame.init()
 	screen = pygame.display.set_mode((500, 350))
 
 	# Titel des Fenster erstellen
-	# Tastendrücke wiederholt senden
+
 	# Mauszeiger unsichtbar machen
 	pygame.display.set_caption("Pong")
-	pygame.key.set_repeat(1, 30)
 	pygame.mouse.set_visible(0)
 
 	# Clock Object für Framerate erstellen
@@ -50,11 +55,18 @@ def main():
 			if event.type == pygame.QUIT:
 				running = False
 
-			# Die gedrückten Tasten bearbeiten
-			if event.type == pygame.KEYDOWN:
-				# Wenn Escape gedrückt wird, dann QUIT-Event versenden
-				if event.key == pygame.K_ESCAPE:
-					pygame.event.post(pygame.event.Event(pygame.QUIT))
+		# Bewegungen bearbeiten
+		if cnt % 10 == 0:
+			state =pygame.key.get_pressed()
+			if state[pygame.K_w]:
+				player_1.move_up()
+			elif state[pygame.K_s]:
+				player_1.move_down()
+			if state[pygame.K_DOWN]:
+				player_2.move_down()
+			elif state[pygame.K_UP]:
+				player_2.move_up()
+
 
 		# schwarzer Hintergrund
 		screen.fill((0,0,0))
@@ -63,6 +75,10 @@ def main():
 		screen.blit(player_1.image, player_1.rect)
 		screen.blit(player_2.image, player_2.rect)
 
+		# den Zähler erhöhen
+		cnt += 1
+
+		# Das Bild aktualisieren
 		pygame.display.flip()
 
 
